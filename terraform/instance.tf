138a729b887285bc2b50449922597c105675d92a
Krix Apolinário <krixapolinario@gmail.com>
2024-11-04 14:49:36 -0300

    adjust meta-data in instance resource

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -4,7 +4,10 @@ resource "google_compute_instance" "lab-sast" {
   machine_type              = var.google_instance_type
   allow_stopping_for_update = true
   zone                      = var.google_zone
-  metadata                  = { ssh-keys = "ansible:${file(var.ssh_key_path_ansible_user)}" }
+  
+  metadata = {
+    ssh-keys = "ansible:${file(var.ssh_key_path_ansible_user)}"
+  }
 
   network_interface {
     network = var.google_network
